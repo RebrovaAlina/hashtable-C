f7bd07ed6074954bdd82e72a620a20553f14dde8
RebrovaAlina <64522511+RebrovaAlina@users.noreply.github.com>
2020-05-16 18:43:43 +0300

    Update utest.c

--- utest.c
+++ utest.c
@@ -2,37 +2,67 @@
 #include "hashtable.h"
 #include <stdlib.h>
 #include <string.h>
-#define HASHTAB_SIZE 128
-#define HASHTAB_MUL 31
 
 int main() {
         ST_ERR err;
         struct listnode *node;
-        //Test 1 - create
         hashtab_init(hashtab);
-        hashtab_add(hashtab, "Tigr", 190,&err);
 
-        //Test2 - collision
-        hashtab_add(hashtab, "Slon", 2300, &err);
-        hashtab_add(hashtab, "Slon", 2300, &err);
+        hashtab_add(hashtab, "Slon", 190,&err);
         hashtab_add(hashtab, "Volk", 60, &err);
+        if (err == STERR_NOADD)
+                fprintf(stdout, "Test_1\t->\tFAILED\n");
+        else
+                fprintf(stdout, "Test_1\t->\tPASSED\n");
+        printf("\n");
 
-        //Test 3 - find in hashtable
         node = hashtab_lookup(hashtab, "Slon",&err);
-        printf("Node: %s, %d\n", node->key, node->value);
-        node = hashtab_lookup(hashtab, "Slon",&err);
-        printf("Node: %s, %d\n", node->key, node->value);
+        if (err == STERR_NOELEM)
+                fprintf(stdout, "Test_2\t->\tFAILED\n");
+        else
+                fprintf(stdout, "Test_2\t->\tPASSED\n");
+        printf("\n");
 
-        //Test 4 - find the element wich is not in the table
         node = hashtab_lookup(hashtab, "Crocodile",&err);
+        if (err == STERR_NOELEM)
+                fprintf(stdout, "Test_3\t->\tFAILED\n");
+        else
+                fprintf(stdout, "Test_3\t->\tPASSED\n");
+        printf("\n");
 
-        //Test 5 - to delete element
         hashtab_delete(hashtab, "Slon", &err);
-        node = hashtab_lookup(hashtab, "Slon",&err);
-        printf("Node: %s, %d\n", node->key, node->value);
+        if (err == STERR_DELETED)
+                fprintf(stdout, "Test_4\t->\tFAILED\n");
+        else
+                fprintf(stdout, "Test_4\t->\tPASSED\n");
+        printf("\n");
 
-        //Test 6 - to delete element wich is not in the table 
+        node = hashtab_lookup(hashtab, "Slon",&err);
+        if (err == STERR_NOELEM)
+                fprintf(stdout, "Test_5\t->\tFAILED\n");
+        else
+                fprintf(stdout, "Test_5\t->\tPASSED\n");
+        printf("\n");
+
         hashtab_delete(hashtab, "Crocodile", &err);
+        if (err == STERR_DELETED)
+                fprintf(stdout, "Test_6\t->\tFAILED\n");
+        else
+                fprintf(stdout, "Test_6\t->\tPASSED\n");
+        printf("\n");
+
+        delete_all(hashtab, &err);
+        if (err == STERR_DELETEDALL)
+                fprintf(stdout, "Test_7\t->\tFAILED\n");
+        else
+                fprintf(stdout, "Test_7\t->\tPASSED\n");
+        printf("\n");
+
+        delete_all(hashtab, &err);
+        if (err == STERR_DELETEDALL)
+                fprintf(stdout, "Test_8\t->\tFAILED\n");
+        else
+                fprintf(stdout, "Test_8\t->\tPASSED\n");
+        printf("\n");
         return 0;
 }
-

5adf2765e3e3372c4e2a8479735cd229e1b1655b
RebrovaAlina <64522511+RebrovaAlina@users.noreply.github.com>
2020-05-16 18:15:40 +0300

    Update hashtable.h

--- hashtable.h
+++ hashtable.h
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-
-#define HASHTAB_SIZE 128 
-#define HASHTAB_MUL 31
+#define HASHTAB_SIZE 128
 typedef enum{
         STERR_SUCCESS=0,
         STERR_FULL  = 1,
         STERR_DELETED= 2,
-        STERR_NOELEM = 3
+        STERR_NOELEM = 3,
+        STERR_DELETEDALL = 4,
+        STERR_NOADD = 5
 } ST_ERR;
 
 struct listnode
@@ -25,3 +25,4 @@ unsigned int hashtab_hash(char *key);
 void hashtab_init(struct listnode **hashtab);
 void hashtab_add(struct listnode **hashtab, char *key, int value, ST_ERR *err) ;
 void hashtab_delete(struct listnode **hashtab, char *key, ST_ERR *err);
+void delete_all(struct listnode **hashtab, ST_ERR *err);

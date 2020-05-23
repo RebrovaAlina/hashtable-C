3d119420a666cb23271f0a85e54ebf8d373ab589
RebrovaAlina <64522511+RebrovaAlina@users.noreply.github.com>
2020-05-24 00:00:24 +0300

    Update utest.c

--- utest.c
+++ utest.c
@@ -10,56 +10,56 @@ int main() {
 
         hashtab_add(hashtab, "Slon", 190,&err);
         hashtab_add(hashtab, "Volk", 60, &err);
-        if (err == STERR_NOADD)
+        if (err != STERR_NOADD&& err!= STERR_SUCCESS)
                 fprintf(stdout, "Test_1\t->\tFAILED\n");
         else
                 fprintf(stdout, "Test_1\t->\tPASSED\n");
         printf("\n");
 
         node = hashtab_lookup(hashtab, "Slon",&err);
-        if (err == STERR_NOELEM)
+        if (err != STERR_NOELEM&& err!= STERR_SUCCESS)
                 fprintf(stdout, "Test_2\t->\tFAILED\n");
         else
                 fprintf(stdout, "Test_2\t->\tPASSED\n");
         printf("\n");
 
         node = hashtab_lookup(hashtab, "Crocodile",&err);
-        if (err == STERR_NOELEM)
+        if (err != STERR_NOELEM && err!= STERR_SUCCESS)
                 fprintf(stdout, "Test_3\t->\tFAILED\n");
         else
                 fprintf(stdout, "Test_3\t->\tPASSED\n");
         printf("\n");
 
         hashtab_delete(hashtab, "Slon", &err);
-        if (err == STERR_DELETED)
+        if (err != STERR_DELETED&& err!= STERR_SUCCESS)
                 fprintf(stdout, "Test_4\t->\tFAILED\n");
         else
                 fprintf(stdout, "Test_4\t->\tPASSED\n");
         printf("\n");
 
         node = hashtab_lookup(hashtab, "Slon",&err);
-        if (err == STERR_NOELEM)
+        if (err != STERR_NOELEM&& err!= STERR_SUCCESS)
                 fprintf(stdout, "Test_5\t->\tFAILED\n");
         else
                 fprintf(stdout, "Test_5\t->\tPASSED\n");
         printf("\n");
 
         hashtab_delete(hashtab, "Crocodile", &err);
-        if (err == STERR_DELETED)
+        if (err != STERR_DELETED&& err!= STERR_SUCCESS)
                 fprintf(stdout, "Test_6\t->\tFAILED\n");
         else
                 fprintf(stdout, "Test_6\t->\tPASSED\n");
         printf("\n");
 
         delete_all(hashtab, &err);
-        if (err == STERR_DELETEDALL)
+        if (err != STERR_DELETEDALL&& err!= STERR_SUCCESS)
                 fprintf(stdout, "Test_7\t->\tFAILED\n");
         else
                 fprintf(stdout, "Test_7\t->\tPASSED\n");
         printf("\n");
 
         delete_all(hashtab, &err);
-        if (err == STERR_DELETEDALL)
+        if (err != STERR_DELETEDALL && err!=STERR_SUCCESS)
                 fprintf(stdout, "Test_8\t->\tFAILED\n");
         else
                 fprintf(stdout, "Test_8\t->\tPASSED\n");
